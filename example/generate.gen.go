1bcf5092873f6b1497b3cb307eaf006104717214
Harry Ford <harry.robert.ford@gmail.com>
2020-07-17 20:58:38 +1000

    Add example

---
+++ example/generate.gen.go
@@ -0,0 +1,33 @@
+package example
+
+import (
+	"encoding/base64"
+	"text/template"
+)
+
+var embeddedTemplates = map[string]string{
+	"example.tmpl": "cGFja2FnZSB7ey5QYWNrYWdlTmFtZX19DQoNCmltcG9ydCAiZm10Ig0KDQpmdW5jIG1haW4oKSB7DQoJZm10LlByaW50bG4oInt7Lk1lc3NhZ2V9fSIpDQp9",
+}
+
+// Parse parses declared templates
+func Parse(t *template.Template) (*template.Template, error) {
+	for name, encoded := range embeddedTemplates {
+		var tmpl *template.Template
+		if t == nil {
+			t = template.New(name)
+		}
+		if name == t.Name() {
+			tmpl = t
+		} else {
+			tmpl = t.New(name)
+		}
+		decoded, err := base64.StdEncoding.DecodeString(encoded)
+		if err != nil {
+			return nil, err
+		}
+		if _, err := tmpl.Parse(string(decoded)); err != nil {
+			return nil, err
+		}
+	}
+	return t, nil
+}
